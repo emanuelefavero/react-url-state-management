273239179008362015d2bf512201565890c8b505
emanuelefavero <emafaux89@gmail.com>
2023-09-19 22:15:40 +0200

    add product page with useState as search products

--- app/products/page.js
+++ app/products/page.js
@@ -1,3 +1,39 @@
+'use client'
+
+import { useState } from 'react'
+
 export default function Page() {
-  return <h1>Products</h1>
+  const PRODUCTS = ['iPhone', 'iPad', 'MacBook', 'AirPods', 'Apple Watch']
+
+  const [query, setQuery] = useState('')
+
+  const filteredProducts = PRODUCTS.filter((product) => {
+    return product.toLowerCase().includes(query.toLowerCase())
+  })
+
+  return (
+    <>
+      <h1>Products</h1>
+
+      {/* SEARCH INPUT */}
+      <label htmlFor='search'>Search</label>
+      <input
+        id='search'
+        type='search'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      {/* SHOW PRODUCTS OR FILTERED PRODUCTS */}
+      {filteredProducts.length > 0 ? (
+        <ul>
+          {filteredProducts.map((product) => {
+            return <li key={product}>{product}</li>
+          })}
+        </ul>
+      ) : (
+        <p>No products found...</p>
+      )}
+    </>
+  )
 }
