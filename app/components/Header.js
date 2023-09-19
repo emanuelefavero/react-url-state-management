3fff92b9b1e24e01045ac9e19522d880b56ac050
emanuelefavero <emafaux89@gmail.com>
2023-09-19 21:53:42 +0200

    add header links, set colors in globals.css

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <>
+      <Link href='/'>Home</Link>
+      <Link href='/products'>Products</Link>
+    </>
+  )
 }
